9fde0c04d523ac1d71405899142b97ac19292c32
RKbhai <raziv9@gmail.com>
2016-09-26 23:34:40 -0400

    verification token

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.listen(3000, function () {
 
 app.get('/webhook', function(req, res) {
   if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+      req.query['hub.verify_token'] === 'gafhbot_project') {
     console.log("Validating webhook");
     res.status(200).send(req.query['hub.challenge']);
   } else {
