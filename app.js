39f11f6285b379076b880b4a59610b574b5808d2
RKbhai <raziv9@gmail.com>
2016-09-27 00:04:14 -0400

    use body parser

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var app = express();
 var bodyParser= require('body-parser');
 var request = require('request');
 
+app.use(bodyParser.json());
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
