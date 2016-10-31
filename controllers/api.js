f32e024e73e972316a67233f56753df0503389a9
Raziv <raziv9@gmail.com>
2016-10-31 18:56:02 -0400

    update main controller file

--- controllers/api.js
+++ controllers/api.js
@@ -1,5 +1,6 @@
 var request     = require('request');
 var properties = require('../config/properties.js');
+var helper = require('../services/WeatherService.js');
 
 //GET request : webhook call
 exports.validateWebhook = function(req, res) {
@@ -13,7 +14,7 @@ exports.validateWebhook = function(req, res) {
     }
 }
 
-//curl to subscribe app, then listen for POST calls at webhook
+//Listen for POST calls at webhook
 exports.handleMessage = function (req, res) {
     var data = req.body;
     console.log("post");
@@ -38,33 +39,22 @@ exports.handleMessage = function (req, res) {
 }
 
 function receivedMessage(event) {
-    //console.log("received");
     var senderID = event.sender.id;
     var recipientID = event.recipient.id;
     var timeOfMessage = event.timestamp;
     var message = event.message;
-
-    //console.log("Received message for user %d and page %d at %d with message:",
-    //senderID, recipientID, timeOfMessage);
-    //console.log(JSON.stringify(message));
-
     var messageId = message.mid;
-    // You may get a text or attachment but not both
     var messageText = message.text;
-console.log(messageText);
+    console.log(messageText);
     // call Wit.AI API to retrieve intent of recieved message
     callWitAI(messageText, senderID);
-        //console.log('intent : ' + intent);
-
-
-
 }
 
 function callWitAI(query, senderID){
   query = encodeURIComponent(query);
     request({
-        uri: 'https://api.wit.ai/message?v=20161003&q='+query,
-        qs:  {access_token: 'IFCCAT4A44BJIK7YZURRKW5RDJQIF4GP'},
+        uri: properties.wit_ai_endpoint + query,
+        qs:  {access_token: properties.wit_ai_token},
         method: 'GET'
       },function(error, response, body){
         if (!error && response.statusCode == 200) {
@@ -76,8 +66,6 @@ function callWitAI(query, senderID){
             }
         } else {
             console.log(response.statusCode);
-            //console.error("Unable to send message %m", error);
-
         }
     });
 }
@@ -85,8 +73,7 @@ function callWitAI(query, senderID){
 function handleIntent(senderID, witai_data){
     try{
         intent = witai_data["entities"]["intent"][0]["value"];
-    }
-    catch(ex){
+    } catch(ex) {
         intent = 'no_intent_found';
     }
     console.log(intent);
@@ -101,8 +88,8 @@ function handleIntent(senderID, witai_data){
                 console.log(lat_lng);
                 getWeatherData(lat_lng, function(err, summary){
                     curr_weather = summary;
-                    console.log("weather : "+curr_weather);
-                    sendTextMessage(senderID, curr_weather);
+                    //console.log("weather : "+curr_weather);
+                    sendTextMessageTest(senderID, curr_weather);
                 });
             });
             break;
@@ -112,12 +99,65 @@ function handleIntent(senderID, witai_data){
     }
 }
 
+function sendTextMessageTest(senderID,body){
+    //Current weather forecast
+    body= JSON.parse(body);
+    var flag_rain_start, flag_rain_stop = 0;
+    summary = body["currently"]["summary"];
+    time1 = body["currently"]["time"];
+    curr_prec = body["currently"]["precipIntensity"];
+    humidity = ((body["currently"]["humidity"])*100).toFixed(2)+" % humid";
+    temperature_f = body["currently"]["temperature"];
+    temperature_c = ((((temperature_f-32)*5)/9).toFixed(2))+" \u00B0C";
+    console.log(summary+' '+temperature_f);
+    curr_weather_data =(summary+", "+temperature_c+", "+humidity).toString();
+
+    //Weather forecast per minute
+    var min_weather_data = '';
+    if(body["minutely"]){
+        icon = body["minutely"]["icon"];
+        data = body["minutely"]["data"];
+        var time2, time_diff_stop, time_diff_start, min_prec_status = 0;
+        if(icon=='rain' || icon=='partly-cloudy-day' || icon=='partly-cloudy-night'){
+            var retdata = helper.prepipitateTime(body, 'minutely');
+            if(retdata){
+                res = retdata.split(':');
+                min_prec_status = res[0];
+                start_stop_time = res[1];
+                min_weather_data = helper.printMessage(min_prec_status, start_stop_time, icon, 'min');
+            }
+        }
+    }
+
+    //Weather forecast per hr
+    var hr_weather_data = '';
+    if(body["hourly"]){
+        icon = body["hourly"]["icon"];
+        data = body["hourly"]["data"];
+        summary_hr = ". "+body["hourly"]["summary"];
+        var time2, time_diff_stop_hr, time_diff_start_hr = 0;
+        if(icon=='snow' || icon=='rain'){
+            if(min_prec_status == 'start' || min_prec_status == 0){
+                var retdata = helper.prepipitateTime(body, 'hourly',min_prec_status);
+                if(retdata){
+                    res = retdata.split(':');
+                    hr_prec_status = res[0];
+                    start_stop_time = res[1];
+                    hr_weather_data = helper.printMessage(hr_prec_status, start_stop_time, icon, 'hr');
+                }
+            }
+        }
+    }
+
+    weather_data = curr_weather_data + summary_hr + min_weather_data + hr_weather_data;
+
+    sendTextMessage(senderID, weather_data);
+}
+
 function getGeolocation(location, callback){
     console.log('location : '+location);
-
-    var API_KEY = "AIzaSyBYOu9ZmhY-QrVGOk4TCQPd2Qzc0b3CSXA";
-    var BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json?address=";
-    var address = "260 Wellesley street, Toronto, Canada";
+    var API_KEY = properties.google_api_token;
+    var BASE_URL = properties.geolocation_endpoint;
     var url = BASE_URL + location + "&key=" + API_KEY;
 
     request(url, function (error, response, body) {
@@ -134,114 +174,29 @@ function getGeolocation(location, callback){
     });
 }
 
-
 //Using Dark Sky API to retrieve weather data
 function getWeatherData(lat_lng, callback){
+    var DARK_SKY_KEY = properties.dark_sky_key;
+    var BASE_URL = properties.dark_sky_endpoint;
+    var url = BASE_URL + DARK_SKY_KEY + "/" + lat_lng;
 
-    request('https://api.darksky.net/forecast/b66ce5b0a9fa37518e4238304b935d43/'+lat_lng,
-        function(error, response, body){
-            if (!error && response.statusCode == 200) {
-                try{
-                    body= JSON.parse(response.body);
+    request(url, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            try{
+                //body= JSON.parse(response.body);
+                callback(null,body);
 
-                    //current weather forecastx
-                    var flag_rain_start,flag_rain_stop = 0;
-                    summary = body["currently"]["summary"];
-                    time1 = body["currently"]["time"];
-                    curr_prec = body["currently"]["precipIntensity"];
-                    humidity = ((body["currently"]["humidity"])*100).toFixed(2)+" % humid";
-                    temperature_f = body["currently"]["temperature"];
-                    temperature_c = ((((temperature_f-32)*5)/9).toFixed(2))+" \u00B0C";
-                    console.log(summary+' '+temperature_f);
-                    curr_weather_data =(summary+", "+temperature_c+", "+humidity).toString();
-
-                    var min_weather_data = '';
-                    //if weather forecast per minute is provided by api
-                    if(body["minutely"]){
-                    //minute weather forecast
-                    icon = body["minutely"]["icon"];
-                    data = body["minutely"]["data"];
-                    var time2,time_diff_stop,time_diff_start = 0;
-                    if(icon=='rain' || icon=='partly-cloudy-day' || icon=='partly-cloudy-night'){
-                        for(var i=0;i<data.length;i++){
-                            time2 = data[i]["time"];
-                            if(time2 > time1){
-                                //rain will stop at :
-                                if(curr_prec > 0 && data[i]["precipIntensity"]==0)
-                                {
-                                    flag_rain_stop = 1;
-                                    time_diff_stop = ((time2-time1)/60).toFixed(0);
-                                    break;
-                                }
-                                //rain will start at :
-                                if(curr_prec == 0 && data[i]["precipIntensity"]>0 && data[i]["precipProbability"]>0.1)
-                                {
-                                    flag_rain_start = 1;
-                                    time_diff_start = ((time2-time1)/60).toFixed(0);
-                                    break;
-                                }
-                            }
-                        }
-                        if(time_diff_start>0){
-                            min_weather_data = " ***** "+icon+" will start in "+time_diff_start+" min";
-                        }
-                        if(time_diff_stop>0){
-                            min_weather_data = " ***** "+icon+" will stop in "+time_diff_stop+" min";
-                        }
-                    }
-                }
-
-                var hr_weather_data = '';
-                //if weather forecast per hr is provided by api
-                if(body["hourly"]){
-                //hr weather forecast
-                icon = body["hourly"]["icon"];
-                data = body["hourly"]["data"];
-                summary_hr = " :-) "+body["hourly"]["summary"];
-                var time2,time_diff_stop_hr,time_diff_start_hr = 0;
-                if(icon=='snow' || icon=='rain'){
-                    for(var i=0;i<data.length;i++){
-                        time2 = data[i]["time"];
-                        if(time2 > time1){
-                            //rain will stop at :
-                            if(curr_prec > 0 && data[i]["precipIntensity"]==0 && flag_rain_stop == 0)
-                            {
-                                time_diff_stop_hr = ((time2-time1)/3600).toFixed(0);
-                                break;
-                            }
-                            //rain will start at :
-                            if(curr_prec == 0 && data[i]["precipIntensity"]>0 && data[i]["precipProbability"]>0.1 && flag_rain_start == 0)
-                            {
-                                time_diff_start_hr = ((time2-time1)/3600).toFixed(0);
-                                break;
-                            }
-                        }
-                    }
-                    if(time_diff_start_hr>0){
-                        hr_weather_data = " ***** "+icon+" will start in "+time_diff_start_hr+" hr";
-                    }
-                    if(time_diff_stop_hr>0){
-                        hr_weather_data = " ***** "+icon+" will stop in "+time_diff_stop_hr+" hr";
-                    }
-                }
+            } catch(ex) {
+                callback(ex);
             }
-
-
-
-                    weather_data = curr_weather_data+summary_hr+min_weather_data+hr_weather_data;
-                    callback(null, weather_data);
-                } catch(ex) {
-                    callback(ex);
-                }
-            } else {
-                console.error("Unable to retrieve weather information %m", error);
-                callback(error);
-            }
+        } else {
+            console.error("Unable to retrieve weather information %m", error);
+            callback(error);
+        }
     });
 }
 
 function sendTextMessage(recipientId, messageText) {
-
     console.log(messageText);
     var messageData = {
         recipient: {
@@ -260,12 +215,10 @@ function callSendAPI(messageData) {
         qs: { access_token: properties.page_token },
         method: 'POST',
         json: messageData
-
     }, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var recipientId = body.recipient_id;
             var messageId = body.message_id;
-
             console.log("Successfully sent generic message with id %s to recipient %s",
             messageId, recipientId);
         } else {
